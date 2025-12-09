7c198e90da13079adceee6250c6bf9c603182bb0
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-12-08 22:27:53 -0500

    Add files via upload

---
+++ ConsoleCommand.cs
@@ -0,0 +1,21 @@
+﻿using Rage;
+using Rage.Attributes;
+
+namespace DisablePistolWhip
+{
+    public static class ConsoleCommand
+    {
+        [ConsoleCommand(Description = "Toggles the Disable Pistol Whip mod on or off.", Name = "dpw")]
+        public static void Toggle()
+        {
+            try
+            {
+                EntryPoint.ToggleEnabled();
+            }
+            catch (System.Exception ex)
+            {
+                Game.LogTrivial($"[Disable Pistol Whip] Console command error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DisablePistolWhip")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JM Modifications")]
+[assembly: AssemblyProduct("DisablePistolWhip")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("05cd81ff-a391-41ce-8296-b8cf754db41a")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
