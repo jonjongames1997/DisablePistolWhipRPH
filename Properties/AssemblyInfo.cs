982d80f64cd62ade681cfef3b53f0e3b6ae94540
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2025-12-19 01:59:55 -0500

    Bump assembly version to 1.0.1.0
    Updated AssemblyVersion and AssemblyFileVersion to 1.0.1.0 to reflect new changes or a minor update.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("05cd81ff-a391-41ce-8296-b8cf754db41a")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
