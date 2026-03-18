ffe9b37bf00cd1691626c30c91dfd50288235318
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2026-03-17 20:55:03 -0400

    Add VersionChecker to check for plugin updates
    Introduce VersionChecker.cs to query the LCPDFR API for the latest plugin version and compare it to Config.PluginVersion. Uses WebClient to download the remote version string, shows in-game notifications and console logs when an update is available or when up-to-date, and handles WebException by notifying the user and logging the failure. Returns a bool indicating whether an update is available. Includes an internal LatestVersion property (currently unused).

---
+++ VersionChecker.cs
@@ -0,0 +1,61 @@
+using Rage;
+using System;
+using System.Net;
+using System.Reflection;
+using DisablePistolWhip;
+
+namespace DisablePistolWhip
+{
+    internal static class VersionChecker
+    {
+        public static object LatestVersion { get; internal set; }
+
+        public static bool IsUpdateAvailable()
+        {
+            string curVersion = Config.PluginVersion;
+            Uri latestVersionUri = new("https://www.lcpdfr.com/applications/downloadsng/interface/api.php?do=checkForUpdates&fileId=52806&textOnly=1");
+            WebClient webClient = new();
+            string recievedData;
+            try
+            {
+                recievedData = webClient.DownloadString(latestVersionUri).Trim();
+            }
+            catch (WebException)
+            {
+                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "~w~Disable Pistol Whip Warning", "~r~Failed to check for an update", "Please make sure you're ~y~connected~w~ to your WiFi Network or try to reload the plugin");
+                Game.Console.Print();
+                Game.Console.Print("===================================================== Disable Pistol Whip ===========================================");
+                Game.Console.Print();
+                Game.Console.Print("[WARNING!]: Failed to check for an update!");
+                Game.Console.Print("[LOG]: Please make sure you are connected to the internet or try to reload the plugin.");
+                Game.Console.Print();
+                Game.Console.Print();
+                Game.Console.Print("==================================================== Disable Pistol Whip ============================================");
+                Game.Console.Print();
+                Game.LogTrivial("Disable Pistol Whip [LOG]: Failed to check for an update. Please make sure you are connected to the internet or try to reload the plugin.");
+                return false;
+            }
+            if (recievedData != Config.PluginVersion)
+            {
+                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "~w~Disable Pistol Whip Warning", "~y~A new update is available!", "Current Version: ~r~" + curVersion + "~w~<br>New Version: ~y~" + recievedData + "<br>~y~Please Update to the latest build for ~p~latest improvments~w~ and ~g~fixes~w~! :-)");
+                Game.Console.Print();
+                Game.Console.Print("===================================================== Disable Pistol Whip ===========================================");
+                Game.Console.Print();
+                Game.Console.Print("[WARNING!]: A new version of Disable Pistol Whip is ~y~NOW AVAILABLE~w~ to download! Update to latest build!");
+                Game.Console.Print("[LOG]: Current Version: " + curVersion);
+                Game.Console.Print("[LOG]: New Version: " + recievedData);
+                Game.Console.Print();
+                Game.Console.Print("");
+                Game.Console.Print();
+                Game.Console.Print("===================================================== Disable Pistol Whip aka `Stop with The F**king Pistol ` ===========================================");
+                Game.Console.Print();
+                return true;
+            }
+            else
+            {
+                Game.DisplayNotification("char_molly", "char_molly", "~w~Disable Pistol Whip Updates", "", "Stella Dimentrescu: We have Detected the ~g~latest~w~ build of ~o~Disable Pistol Whip~w~! Thank you for your business, citizen!");
+                return false;
+            }
+        }
+    }
+}
